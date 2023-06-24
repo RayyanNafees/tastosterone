9f2bd6d0676d143ebcd867e7cd0cbec46e368cff
RayyanNafees <nafees.rayyan@gmail.com>
2023-06-24 08:40:36 +0530

    Fixed some stuff

--- client/src/pages/BranchAdminReg/BranchAdminSharedLayout.jsx
+++ client/src/pages/BranchAdminReg/BranchAdminSharedLayout.jsx
@@ -1,17 +1,17 @@
-'use client'
 import React, { useState } from 'react'
-import tasLogo from '@/app'
+import { Outlet } from 'react-router-dom'
+import tasLogo from '../../assets/Logo.png'
 
 import { BsFacebook, BsTwitter, BsLinkedin } from 'react-icons/bs'
 
-import { UserDataContext } from '@/app/data/globalData'
+import { UserDataContext } from '../../data/globalData'
 
 const BranchAdminSharedLayout = () => {
   const [regData, setRegData] = useState({})
   const SideBar = () => {
     return (
       <aside className='reg-page-aside xs:hidden sm:inline sm:w-3/12 sm:h-[screen]'>
-        <div className='flex flex-col justify-between w-full h-screen px-2 py-3 reg-page-aside-transparent'>
+        <div className='reg-page-aside-transparent w-full h-screen px-2 py-3 flex flex-col justify-between'>
           <img src={tasLogo} className='xs:hidden sm:inline w-28' />
           <span className='font-inter text-white tracking-[-0.02rem] leading-7 flex flex-col gap-3 xs:row-start-1 xs:row-end-2 sm:row-start-3 sm:row-end-4'>
             <h2 className=' font-semibold text-[28px] text-left '>
@@ -22,14 +22,14 @@ const BranchAdminSharedLayout = () => {
             </p>
           </span>
 
-          <footer className='text-white xs:row-start-4 xs:row-end-5 sm:row-start-6 sm:row-end-7'>
+          <footer className='xs:row-start-4 xs:row-end-5 sm:row-start-6 sm:row-end-7 text-white'>
             <span className='flex gap-2 '>
               <BsTwitter />
               <BsFacebook />
               <BsLinkedin />
             </span>
 
-            <span className='flex gap-3 mt-6 text-sm'>
+            <span className='flex gap-3 text-sm mt-6'>
               <p>Terms of service</p>
               <p>Privacy policy</p>
               <p>Help</p>
@@ -44,6 +44,9 @@ const BranchAdminSharedLayout = () => {
     <UserDataContext.Provider value={{ regData, setRegData }}>
       <div className='org-reg-body font-openSans '>
         <SideBar />
+        <div className='overflow-hidden flex flex-col sm:w-9/12'>
+          <Outlet />
+        </div>
       </div>
     </UserDataContext.Provider>
   )
